ed80b671ee6997844f766c31ebadcf385597d9fe
cxb767 <cekck5@gmail.com>
2026-07-15 20:07:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'running-v5';
+const CACHE = 'running-v6';
 const STATIC = ['/weather/', '/weather/index.html', '/weather/manifest.json', '/weather/icon-192.png', '/weather/icon-512.png'];
 
 self.addEventListener('install', e => {
