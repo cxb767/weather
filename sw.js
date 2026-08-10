bf14dd3c930431b55e11759bcb713e0c003ba384
cxb767 <cekck5@gmail.com>
2026-08-10 14:06:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weather-v27';
+const CACHE = 'weather-v29';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => {
